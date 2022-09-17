102184cd3208a671c594c9ba509935e8f1f281ef
Mohid123 <muhammadmohid141@gmail.com>
2022-09-17 16:55:18 +0500

    new build

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 export default { 
-    mongoURI: 'mongodb://localhost:27017/Blog_Backend',
+    mongoURI: 'mongodb://127.0.0.1:27017/Blog_Backend',
     SECRET_KEY: 'MySuperSecretKeyIsSuperSecret',
+    URL:'https://animetography-blog.com/',
     whiteListedExtensions: ['.apng', '.avif', '.gif', '.jpg', '.jpeg', '.jfif', '.pjpeg', '.pjp', '.png', '.svg','.webm', '.mkv', '.flv', '.vob', '.ogg', '.ogv', '.gif', '.mov', '.wmv', '.3gp', '.mp4', '.m4p', '.m4v']
 }
