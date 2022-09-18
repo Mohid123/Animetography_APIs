9b9a7e1dc86de4017c76ccc1797bd0c2512252b8
Mohid123 <muhammadmohid141@gmail.com>
2022-09-18 17:27:19 +0500

    latest build

--- dist/config.js
+++ dist/config.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     mongoURI: 'mongodb://127.0.0.1:27017/Blog_Backend',
     SECRET_KEY: 'MySuperSecretKeyIsSuperSecret',
-    URL: 'https://animetography-blog.com/',
+    URL: 'https://api.animetography-blog.com/',
     whiteListedExtensions: ['.apng', '.avif', '.gif', '.jpg', '.jpeg', '.jfif', '.pjpeg', '.pjp', '.png', '.svg', '.webm', '.mkv', '.flv', '.vob', '.ogg', '.ogv', '.gif', '.mov', '.wmv', '.3gp', '.mp4', '.m4p', '.m4v']
 };
 //# sourceMappingURL=config.js.map
