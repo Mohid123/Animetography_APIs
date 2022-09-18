1496101bb8e0a59e1773202c8714c2159a420a90
Mohid123 <muhammadmohid141@gmail.com>
2022-09-18 20:35:58 +0500

    change base url for media upload

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 export default { 
     mongoURI: 'mongodb://127.0.0.1:27017/Blog_Backend',
     SECRET_KEY: 'MySuperSecretKeyIsSuperSecret',
-    URL:'https://api.animetography-blog.com/',
+    URL:'https://api.animetography-blog.com/api/',
     whiteListedExtensions: ['.apng', '.avif', '.gif', '.jpg', '.jpeg', '.jfif', '.pjpeg', '.pjp', '.png', '.svg','.webm', '.mkv', '.flv', '.vob', '.ogg', '.ogv', '.gif', '.mov', '.wmv', '.3gp', '.mp4', '.m4p', '.m4v']
 }
